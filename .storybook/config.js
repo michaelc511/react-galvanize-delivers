601ef59ef965573366c8d7741d173b6d565a9701
michaelc511 <michaelc511@gmail.com>
2017-08-29 09:51:28 -0700

    Added .storybook folder w config.js

---
+++ .storybook/config.js
@@ -0,0 +1,24 @@
+import { configure } from '@storybook/react';
+function loadStories() {
+  require('../src/index.css');
+  // Require your stories here...
+
+  // 1 MenuItemComponent
+  require('../src/components/MenuItemComponent.story');
+
+  // 2 MenuComponent
+  require('../src/components/MenuComponent.story');
+
+  // 3 OrderTableComponent
+  require('../src/components/OrderTableComponent.story');
+
+  // 4 OrderFormComponent
+  require('../src/components/OrderFormComponent.story');
+
+  // 5 OrderPageLayout
+  require('../src/components/OrderPageLayout.story');
+
+  // 6 OrderPage
+  require('../src/components/OrderPage.story');
+}
+configure(loadStories, module);
